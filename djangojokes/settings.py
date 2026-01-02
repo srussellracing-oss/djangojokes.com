a32b53523ea2b0dc0edbaadc63b0a04710f8054d
Steven Russell <srussellracing@gmail.com>
2026-01-02 13:06:39 -0600

    update

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
         # Local apps
+    'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
 ]
