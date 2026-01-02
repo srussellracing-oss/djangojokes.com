c184951b903c93009297df3245e3fb50c55e3569
Steven Russell <srussellracing@gmail.com>
2026-01-02 15:00:06 -0600

    update

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'Mrdude91!',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
@@ -119,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
