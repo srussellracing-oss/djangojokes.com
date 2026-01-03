59dcff49423d1d720a7f42e7329a8ffe1eda403e
Steven Russell <srussellracing@gmail.com>
2026-01-03 11:35:36 -0600

    update

--- jokes/models.py
+++ jokes/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.urls import reverse
+from common.utils.text import unique_slug
 
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(
+    max_length=50, unique=True, null=False, editable=False
+    
+    )
+    def get_absolute_url(self):
+        return reverse("jokes:joke_detail", args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
 
-    def get_absolute_url(self): return reverse("jokes:joke_detail", args=[self.pk])
-
+        super().save(*args, **kwargs)
     def __str__(self):
         return self.question
