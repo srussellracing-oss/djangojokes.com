cfccd9506567d435daa059282c9001bb71a8c04e
Steven Russell <srussellracing@gmail.com>
2026-01-02 18:24:34 -0600

    update

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -42,6 +43,8 @@ INSTALLED_APPS = [
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
+
+    
 ]
 
 MIDDLEWARE = [
